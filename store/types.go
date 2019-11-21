3b66b6c4d41263ddcc1925b7922b80f3f5ddd812
Luke Williams <luke@iquidus.io>
2019-11-21 11:22:31 +0100

    store: add types (state)

---
+++ store/types.go
@@ -0,0 +1,9 @@
+package store
+
+type State struct {
+	Address  string `bson:"address" json:"address"`
+	Mnemonic string `bson:"mnemonic" json:"mnemonic"`
+	HdPath   string `bson:"hdPath" json:"hdPath"`
+	ChainID  string `bson:"chainID" json:"chainID"`
+	Node     string `bson:"node" json:"node"`
+}
