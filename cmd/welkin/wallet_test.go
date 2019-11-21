9579d9a65a782d17c5c5f4c59ebfe61a371b90f9
Luke Williams <luke@iquidus.io>
2019-11-21 14:23:18 +0100

    wallet: update tests

--- cmd/welkin/wallet_test.go
+++ cmd/welkin/wallet_test.go
@@ -5,9 +5,13 @@ import (
 	"testing"
 )
 
+const (
+	mnemonic = "walnut tower salon print rural upper all always year video alarm empty"
+)
+
 func TestGenerateMnemonic(t *testing.T) {
-	mnemonic := GenerateMnemonic()
-	arr := strings.Split(mnemonic, " ")
+	tMnemonic := GenerateMnemonic()
+	arr := strings.Split(tMnemonic, " ")
 	len := len(arr)
 	if len != 24 {
 		t.Fatal("invalid number of words in mnemonic")
@@ -16,11 +20,23 @@ func TestGenerateMnemonic(t *testing.T) {
 }
 
 func TestCreateWallet(t *testing.T) {
-	mnemonic := "walnut tower salon print rural upper all always year video alarm empty"
 	wallet := CreateWallet(mnemonic)
 	address := GetAddressFromPath(wallet, "0")
 	if address != "0xf176F44A36A1F81f8aD2723Db64b8751F6235D2d" {
 		t.Fatal("0xf176F44A36A1F81f8aD2723Db64b8751F6235D2d not found at position 0")
+	}
+	return
+}
+
+func TestGetAddressFromPath(t *testing.T) {
+	wallet := CreateWallet(mnemonic)
+	address := GetAddressFromPath(wallet, "0")
+	if address != "0xf176F44A36A1F81f8aD2723Db64b8751F6235D2d" {
+		t.Fatal("0xf176F44A36A1F81f8aD2723Db64b8751F6235D2d not found at position 0")
+	}
+	address = GetAddressFromPath(wallet, "1")
+	if address != "0xDd2717dfe605a52EeD8eDe92Ae5D44AbD2c7eD36" {
+		t.Fatal("0xDd2717dfe605a52EeD8eDe92Ae5D44AbD2c7eD36 not found at position 1")
 	}
 	return
 }
